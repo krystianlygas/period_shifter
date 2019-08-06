27b81b4275ad23f7a6ca62cc34d504b2eb216d79
krystian.lygasgit <krystian.lygas@gmail.com>
2019-08-06 11:09:31 +0200

    Version before changing User field

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,16 +1,13 @@
 $(function() {
     $.get('http://localhost:8000/users/'+$.cookie('id')).done(function(data, status){
-        
         $('#userDropdown span').text(`${data.first_name} ${data.last_name}`);
         // alert("Data: " + data.first_name + data.last_name + "\nStatus: " + status);
-
         $.get(data.userprofile).done(function(data,status){
             $('.img-profile').attr('src',(data.profile_image))
         })
       }).fail(function() {
         window.location.href = "login.html";
       });
-   
       ($('.modal-footer .btn')).on('click', function(event){
           if ($.cookie('token')){
           $.removeCookie('token')
